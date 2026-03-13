8eeb325f60d6588f57a7225acc737688523b995e
enoch-bolarinwa <enoch-bolarinwa@>
2026-03-13 12:05:38 +0100

    server

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Now you can access environment variables anywhere in your code
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+
+console.log(`Connecting to database at ${dbHost} with user ${dbUser}`);
+
+// Your other application code follows
+// For example, setting up your server, routes, etc.
+
+
 const express = require('express');
+const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
 const app = express();
-const routes = require('./routes');
+const port = process.env.PORT || 3002;
 
-const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use('/', require('./routes'))
 
-// Use routes
-app.use('/', routes);
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else{app.listen(port, () => {console.log(`database is listening and node Running on port ${port}`)});
+}
 });
