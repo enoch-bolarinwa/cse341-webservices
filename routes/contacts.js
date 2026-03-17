3a30061127ced48e0d046bd03ca798444df4e8d4
enoch-bolarinwa <enoch-bolarinwa@>
2026-03-17 09:44:03 +0100

    contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,19 +2,161 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Contact:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - email
+ *         - favoriteColor
+ *         - birthday
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: Auto-generated MongoDB ID
+ *         firstName:
+ *           type: string
+ *           description: Contact's first name
+ *         lastName:
+ *           type: string
+ *           description: Contact's last name
+ *         email:
+ *           type: string
+ *           description: Contact's email address
+ *         favoriteColor:
+ *           type: string
+ *           description: Contact's favorite color
+ *         birthday:
+ *           type: string
+ *           description: Contact's birthday
+ *       example:
+ *         firstName: John
+ *         lastName: Doe
+ *         email: john.doe@example.com
+ *         favoriteColor: Blue
+ *         birthday: 1990-01-15
+ */
 
+/**
+ * @swagger
+ * /contacts:
+ *   get:
+ *     summary: Get all contacts
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: List of all contacts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Contact'
+ */
 router.get('/', contactsController.getAllContacts);
 
-
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   get:
+ *     summary: Get contact by ID
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Contact ID
+ *     responses:
+ *       200:
+ *         description: Contact found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Contact'
+ *       404:
+ *         description: Contact not found
+ */
 router.get('/:id', contactsController.getSingleContact);
 
-
+/**
+ * @swagger
+ * /contacts:
+ *   post:
+ *     summary: Create new contact
+ *     tags: [Contacts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Contact'
+ *     responses:
+ *       201:
+ *         description: Contact created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *       500:
+ *         description: Server error
+ */
 router.post('/', contactsController.createContact);
 
-
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   put:
+ *     summary: Update contact
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Contact ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Contact'
+ *     responses:
+ *       204:
+ *         description: Contact updated successfully
+ *       404:
+ *         description: Contact not found
+ */
 router.put('/:id', contactsController.updateContact);
 
-
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   delete:
+ *     summary: Delete contact
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Contact ID
+ *     responses:
+ *       200:
+ *         description: Contact deleted successfully
+ *       404:
+ *         description: Contact not found
+ */
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
