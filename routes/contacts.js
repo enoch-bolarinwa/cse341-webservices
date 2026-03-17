c5ae3af8bf2cfc67f75507c6fd7ba5df7b3facb1
enoch-bolarinwa <enoch-bolarinwa@>
2026-03-17 09:38:12 +0100

    contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
-// Routes WITHOUT Swagger comments for now (to test if routes work)
+
 router.get('/', contactsController.getAllContacts);
+
+
 router.get('/:id', contactsController.getSingleContact);
+
+
 router.post('/', contactsController.createContact);
+
+
 router.put('/:id', contactsController.updateContact);
+
+
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
